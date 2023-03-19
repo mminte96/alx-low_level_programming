787a74c0ce21d5685c4cd8db9f02e5385e387d26
mminte96 <mbminthe1996malang@gmail.com>
2023-03-19 06:38:47 -0700

    all digit nunbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print all single nunbers of base 10
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}	
