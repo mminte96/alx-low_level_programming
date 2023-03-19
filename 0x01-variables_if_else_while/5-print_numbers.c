a3f4d030bbb6e5e02415bddf42ed6fbe3fc2352d
mminte96 <mbminthe1996malang@gmail.com>
2023-03-19 06:18:47 -0700

    all digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 00; i <= 09; i++)
+	{
+		printf(i);
+	}
+	return (0);
+}
