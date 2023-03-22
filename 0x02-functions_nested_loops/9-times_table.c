a8ac59032de09bfa4f2471443665cf9b80fc4a26
mminte96 <mbminthe1996malang@gmail.com>
2023-03-21 18:14:01 -0700

    tables times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				-putchar((c / 10) + '0');
+				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
 			}
 			_putchar('\n');
