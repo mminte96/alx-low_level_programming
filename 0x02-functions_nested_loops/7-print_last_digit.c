f3651213091d8f8015d0f44409ed785f31b77a1f
mminte96 <mbminthe1996malang@gmail.com>
2023-03-21 17:09:22 -0700

    last digit print

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_last_digit - print the las digit of number
+ * @n: will be checked
+ * Return: Always (Success)
+ */
+int print_last_digit(int n)
+{
+	int m;
+
+	if (n < 0)
+	{
+		n = -n;
+	}
+	m = n % 10;
+	if (m < 0)
+	{
+		m = -m;
+		_putchar(m + '0');
+	}
+	return (m);
+}
