bc2f2ce93e6b16da17fcdd375336a8e540bafae0
mminte96 <mbminthe1996malang@gmail.com>
2023-04-03 18:18:59 -0700

    life is a ragged diagonal

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square matrix
+ * @a: input value
+ * @size: size of input
+ * Return: always 0
+ */
+void print_diagsums(int *a, int size)
+{
+	int m;
+	int th;
+	int sum0 = 0;
+	int sum1 = 0;
+
+	for (m = 0; m <= (size * size); m = m + size - 1)
+	{
+		sum0 = sum0 + a[m];
+	}
+	for (th = size - 1; th <= (size * size) - size; th = th + size - 1)
+	{
+		sum1 = sum1 + a[th];
+	}
+	printf("%d, %d\n", sum0, sum1);
+}
