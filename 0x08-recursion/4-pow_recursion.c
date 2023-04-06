87e327a17f9adec096a9c552512cb35edc7d0aa0
mminte96 <mbminthe1996malang@gmail.com>
2023-04-05 17:45:45 -0700

    recursive power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,13 +7,16 @@
  */
 int _pow_recursion(int x, int y)
 {
+	int p;
+
 	if (y < 0)
 	{
 		return (-1);
 	}
 	if (y <= 1)
 	{
-		return (x, y);
+		return (x);
 	}
-	return (x ^ _pow_recursion(x, y));
+	p = (x, y);
+	return (x ^ _pow_recursion(p));
 }
