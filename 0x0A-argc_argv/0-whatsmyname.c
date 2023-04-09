40a73c57f62f60c4ca4e90f57d0d0a39e7ea3725
mminte96 <mbminthe1996malang@gmail.com>
2023-04-09 11:25:59 -0700

    name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - a program that prints its name
+ * @argc: input value
+ * @argv: input value
+ * Return: Always (success)
+ */
+int main(int argc__attribute__((unused)) char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
