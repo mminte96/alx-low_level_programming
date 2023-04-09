20327da02df0774652be577cb18b10e2bd425510
mminte96 <mbminthe1996malang@gmail.com>
2023-04-09 13:51:31 -0700

    prints all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - a program that prints all arguments it receives
+ * @argc: input value
+ * @argv: input value
+ */
+int main(int argc, char *argv[])
+{
+	int m;
+
+	for (m = 0; m < argc; m++)
+	{
+		printf("%s\n", argv[m]);
+	}
+	return (0);
+}
