aba1b4d3e86595c6018f41a270484c884b60aaf1
mminte96 <mbminthe1996malang@gmail.com>
2023-04-17 13:21:35 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *  main - Define a new type struct dog
+ *  @struct dog: a structure
+ *  @name: element of struct
+ *  @age: another element
+ *  @owner: last element
+ *  Return: Always 0 (success)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+int main(void)
+{
+	struct dog dog;
+
+	return (0);
+}
+
