6a455c55847c7cca8ddffb6dd173fd62253061cc
mminte96 <mbminthe1996malang@gmail.com>
2023-04-17 17:16:52 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef STRUCT_DOG
 #define STRUCT_DOG
-
+#include "main.h"
+/**
+ * srtuct dog - Define a new type struct
+ * @name: type of stuct
+ * @age: type float
+ * @owner: another char
+ * Description: longer description
+ */
 struct dog
 {
 	char *name;
