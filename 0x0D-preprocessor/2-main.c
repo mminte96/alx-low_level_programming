d90ae64c5be9ce16e5979434919d1ba8a432a686
mminte96 <mbminthe1996malang@gmail.com>
2023-04-18 07:07:40 -0700

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a fontion that print the name of file
+ * Return: EXIT_SUCCESS
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (EXIT_SUCCESS);
+}
