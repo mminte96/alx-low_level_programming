3b1b976d892b4bbe1d4f57c2f823214daa6726b0
mminte96 <mbminthe1996malang@gmail.com>
2023-04-19 09:36:56 -0700

    int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,13 +5,13 @@
  * @array: an array of function
  * @size: another int
  * @cmp: second function
- * Return: 0
+ * Return: -1
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int m;
 
-	if (array == NULL || size == NULL || cmp == NULL)
+	if (array == NULL || size <= 0 || cmp == NULL)
 	{
 		return (-1);
 	}
